dfb4c8af511a757073132030a86b804bae4f010d
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-03 13:29:20 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
   
+// }
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
